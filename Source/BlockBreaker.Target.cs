52812b61557b884dc7430cf736719260bf978d70
johnche <johnche@tencent.com>
2024-10-31 15:12:53 +0800

    ue5.4

--- Source/BlockBreaker.Target.cs
+++ Source/BlockBreaker.Target.cs
@@ -8,7 +8,12 @@ public class BlockBreakerTarget : TargetRules
 	public BlockBreakerTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
+#if UE_5_4_OR_LATER
+		DefaultBuildSettings = BuildSettingsVersion.V5;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_4;
+#else
+        DefaultBuildSettings = BuildSettingsVersion.V2;
+#endif
 
 		ExtraModuleNames.AddRange( new string[] { "BlockBreaker" } );
 	}
--- Source/BlockBreakerEditor.Target.cs
+++ Source/BlockBreakerEditor.Target.cs
@@ -8,7 +8,12 @@ public class BlockBreakerEditorTarget : TargetRules
 	public BlockBreakerEditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
+#if UE_5_4_OR_LATER
+		DefaultBuildSettings = BuildSettingsVersion.V5;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_4;
+#else
+        DefaultBuildSettings = BuildSettingsVersion.V2;
+#endif
 
 		ExtraModuleNames.AddRange( new string[] { "BlockBreaker" } );
 	}
